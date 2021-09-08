286d98ad93aebcdc399d625bc6abe118f5af4551
Golodkov Den <gold8523@.gmail.com>
2021-09-08 18:54:32 +0300

    react components & styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import bg1 from './assets/bg1.jpg';
+import bg3 from './assets/bg3.jpg';
 
 function App() {
+  const title = 'This is title';
+  const descr = 'This is Description!'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first app on react :)
-        </p>
-      </header>
-    </div>
+    <>
+      <Header
+        title = {title}
+        descr = {descr}
+      />
+      <Layout
+        title = {title}
+        descr={descr}
+        urlBg = {bg1}
+      />
+      <Layout
+        title = {title}
+        descr={descr}
+        colorBg = '#30d5c8'
+      />
+      <Layout
+        title = {title}
+        descr={descr}
+        urlBg = {bg3}
+      />
+      <Footer/>
+    </>
   );
 }
 
