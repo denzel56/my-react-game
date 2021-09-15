02042c6cacb9c9bac348bac4b7f30654df278d41
Golodkov Den <gold8523@.gmail.com>
2021-09-15 17:32:15 +0300

    код из 3 видео

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import { useState } from "react";
-import GamePage from "./routes/Game";
-import HomePage from "./routes/Home";
+import { useRouteMatch, Route, Switch } from "react-router";
+import cn from 'classnames';
+import HomePage from "./routes/HomePage";
+import GamePage from "./routes/GamePage";
+import MenuHeader from "./components/MenuHeader";
+import Footer from "./components/Footer";
+
+import s from "./style.module.css";
 
 const App = () => {
-  const [page, setState] = useState('app');
+  const match = useRouteMatch('/');
 
-  const handleChangePage = (page) => {
-    setState(page);
-  }
-
-  switch (page) {
-    case "app":
-      return <HomePage onChangePage = {handleChangePage}/>
-    case "game":
-      return <GamePage onChangePage = {handleChangePage}/>
-    default:
-      return <HomePage />
-  }
+  return (
+    <Switch>
+      <Route>
+        <>
+          <MenuHeader bgActive={!match.isExact} />
+          <div className={cn(s.wrap, {
+            [s.isHomePage]: match.isExact
+          })}>
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/home" component={HomePage} />
+              <Route path="/game" component={GamePage} />
+              <Route path="/about" render={() => {
+                <h1> This is page About </h1>
+              }} />
+            </Switch>
+          </div>
+          <Footer />
+        </>
+      </Route>
+    </Switch>
+  )
 }
 
 export default App;
