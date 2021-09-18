45907d9684ea3507ef71d64c9279f2b968e968c2
Golodkov Den <gold8523@.gmail.com>
2021-09-18 22:48:49 +0300

    get data from database

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,37 +1,55 @@
-import { useState } from "react";
-import POKEMONS from '../../data/pokemons.json';
+import { useState, useEffect } from "react";
+import { database } from "../../service/firebase";
+import { get, child, ref, set } from "firebase/database";
+// import POKEMONS from '../../data/pokemons.json';
 
 import PokemonCards from "../../components/PokemonCards";
 
 import s from './style.module.css';
 
 const GamePage = (isActiveId) => {
-  const [pokemons, setPokemonsAtive] = useState(POKEMONS)
+  const [pokemons, setPokemonsAtive] = useState({})
+
+  useEffect(() => {
+    const dbRef = ref(database);
+    get(child(dbRef, `pokemons`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        setPokemonsAtive(snapshot.val());
+        console.log('#### data', snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, [])
 
   const handleClick = (isActiveId) => {
     setPokemonsAtive(prevState => {
-      prevState.map((item) => {
-        if (item.id === isActiveId) {
-          item.active = !item.active
-        }
+      return Object.entries(prevState).reduce((acc, item) => {
+        const pokemon = { ...item[1] };
+        if (pokemon.id === isActiveId) {
+          pokemon.active = true;
+        };
+        console.log('### id click', isActiveId);
+        acc[item[0]] = pokemon;
 
-      })
-      return [...prevState];
+        return acc;
+      }, {});
     })
   }
-
   return (
     <>
       <div className={s.flex}>
         {
-          pokemons.map((item) => <PokemonCards
-            key={item.id}
-            name={item.name}
-            img={item.img}
-            id={item.id}
-            type={item.type}
-            values={item.values}
-            isActive = {item.active}
+          Object.entries(pokemons).map(([key, {name, img, id, type, values, active}]) => <PokemonCards
+            key={key}
+            name={name}
+            img={img}
+            id={id}
+            type={type}
+            values={values}
+            isActive = {active}
             onClickCard={handleClick}
           />)
         }
